bb0c026ce920c4f1e0717c452e86ad65eb0696ca
ThomasFaria <thomas.faria@ensae.fr>
2024-03-18 17:33:01 +0000

    remove token

--- s3-token-satellite-images.sh
+++ s3-token-satellite-images.sh
@@ -1,7 +1,6 @@
 # S3
 AWS_ACCESS_KEY_ID=`vault kv get -field=ACCESS_KEY_ID onyxia-kv/projet-slums-detection/s3` && export AWS_ACCESS_KEY_ID
 AWS_SECRET_ACCESS_KEY=`vault kv get -field=SECRET_ACCESS_KEY onyxia-kv/projet-slums-detection/s3` && export AWS_SECRET_ACCESS_KEY
-unset AWS_SESSION_TOKEN
 
 # MC
 export MC_HOST_s3=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
